981095d5e9dedb9696dacef7f8554dfdc4aa3d0c
jdollar <joel.dollarhide@passportinc.com>
2017-01-08 11:41:07 -0500

    Setup so that it will generate a list with a single item for the main menu

--- resources/lib/kissanime/kissanime.py
+++ resources/lib/kissanime/kissanime.py
@@ -1,7 +1,21 @@
+import sys
+import xbmcplugin
+import xbmcgui
+
+ADDON_HANDLE = int(sys.argv[1])
+
 class KissAnime:
     def __init__(self):
-        test = "test"
-        test += test
+        xbmcplugin.setContent(ADDON_HANDLE, 'movies')
+    # End init
 
     def run(self):
-        return "true"
+        self.buildMainMenu()
+    # End run
+
+    def buildMainMenu(self):
+        li = xbmcgui.ListItem('allVideos', iconImage='DefaultVideo.jpg')
+        xbmcplugin.addDirectoryItem(handle=ADDON_HANDLE, url=None, listitem=li, isFolder=True)
+        xbmcplugin.endOfDirectory(ADDON_HANDLE)
+    #End buildMainMenu
+# End KissAnime
