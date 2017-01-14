064f8ee1bf94bd5e4a497596ae1cd38097ba3889
jdollar <joel.dollarhide@passportinc.com>
2017-01-13 22:09:19 -0500

    Added a constants file and cleaned up the addon.xml file

---
+++ lib/kissanime/constants.py
@@ -0,0 +1,4 @@
+KISS_ANIME_BASE_URL = 'http://kissanime.ru/'
+ANIME_LIST_ENDPOINT = 'AnimeList'
+CONTENT_TYPE_MOVIES = 'movies'
+
--- lib/kissanime/kissanime.py
+++ lib/kissanime/kissanime.py
@@ -1,20 +1,16 @@
 import sys
-import urllib2
 import xbmcplugin
 import xbmcgui
-import js2py
-import cfscrape
 
+import cfscrape
 from bs4 import BeautifulSoup
-from t0mm0.common.net import Net
+from constants import *
 
 ADDON_HANDLE = int(sys.argv[1])
-KISS_ANIME_BASE_URL = 'http://kissanime.ru/'
-ANIME_LIST_ENDPOINT = 'AnimeList'
 
 class KissAnime:
     def __init__(self):
-        xbmcplugin.setContent(ADDON_HANDLE, 'movies')
+        xbmcplugin.setContent(ADDON_HANDLE, CONTENT_TYPE_MOVIES)
     # End init
 
     def run(self):
